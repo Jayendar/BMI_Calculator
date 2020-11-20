3eac18cefd853b551e60c7adee7ac0868d488ca8
Jayendar <jayendar.a@gmail.com>
2020-11-20 20:18:33 +0530

    Initial Commit

---
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,48 @@
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    var calculatorBrain = CalculatorBrain()
+
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text =  String(format: "%.2f m", sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0f kg", sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        calculatorBrain.calculateBMI(height: height, weight: weight)
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+        
+//        let secondVC = SecondViewController()
+//        secondVC.bmiValue = String(format: "%.1f", totalBMI)
+//        self.present(secondVC, animated: true, completion: nil) //present the secondViewController once the calculate button is pressed
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController // destinationVC and segue.destination has a class of UIViewContrller
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
+            
+        }
+    }
+}
+
